ff056460b70460090b4c9c46992665331ff68c2e
fsajadi <s.sfatemeh@gmail.com>
2019-10-13 18:53:35 -0400

    delete unnecessary components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { from } from 'rxjs';
-import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component'
-import { SuccessAlertComponent } from './SuccessAlert/SuccessAlert.component'
-import { WarningAlertComponent } from './WarningAlert/WarningAlert.component';
-import { ToDoItemComponent } from './ToDoItem/ToDoItem.component';
+
 import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
